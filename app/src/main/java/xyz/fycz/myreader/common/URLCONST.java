61b73a437184058d0ff1017069a1e421d1922380
fengyue <1021300691@qq.com>
2021-01-24 19:56:56 +0800

    修复替换规则删除bug

--- app/src/main/java/xyz/fycz/myreader/common/URLCONST.java
+++ app/src/main/java/xyz/fycz/myreader/common/URLCONST.java
@@ -5,26 +5,7 @@ public class URLCONST {
     // 命名空间
     public static String nameSpace_FY = "https://novel.fycz.xyz";
 
-    public static String nameSpace_biquge = "https://www.52bqg.com/";
-
-    public static String nameSpace_qb5 = "https://www.qb5.tw";
-
-    public static String qidian_rank = "https://www.qidian.com/rank";
-
-    public static String nameSpace_system = "https://novel.fycz.xyz";
-
     public static boolean isRSA = false;
-
-    // 搜索小说
-    public static String method_fengyue_search = "https://novel.fycz.xyz/search.html";
-
-    public static String method_buxiu_search = "https://www.23txt.com/search.php";
-
-    // 获取最新版本号
-    public static String method_getCurAppVersion = nameSpace_system + "/mReaderController.do?getCurAppVersion";
-
-    // app下载
-    public static String method_downloadApp = nameSpace_system + "/app/FYReader.apk";
 
     public static String APP_DIR_UR = "https://www.lanzous.com/b00ngso7e";
 
