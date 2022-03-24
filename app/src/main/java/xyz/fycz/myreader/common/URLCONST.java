ba4d40b5ce62cf7b55a863424c7118427605e8fb
fengyuecanzhu <1021300691@qq.com>
2022-03-24 15:51:15 +0800

    change thanks url

--- app/src/main/java/xyz/fycz/myreader/common/URLCONST.java
+++ app/src/main/java/xyz/fycz/myreader/common/URLCONST.java
@@ -23,12 +23,11 @@ public class URLCONST {
 
     public static final String YOU_DAO_SEARCH = "http://m.youdao.com/dict?le=eng&q={key}";
 
-
     public static final String FY_READER_URL = "https://fyreader." + getDefaultDomain();
 
     public static final String USER_URL = "http://101.43.83.105:12123";
     public static final String LOG_UPLOAD_URL = USER_URL + "/do/logUpload";
-    public static final String THANKS_URL = FY_READER_URL + "/thanks/";
+    public static final String THANKS_URL = USER_URL + "/thanks/";
     public static final String AD_URL = USER_URL + "/do/ad";
 
     public static final String DONATE = "https://gitee.com/fengyuecanzhu/Donate/raw/master";
@@ -40,7 +39,7 @@ public class URLCONST {
     public static final String QUOTATION = "https://v1.hitokoto.cn/?encode=json&charset=utf-8";
 
     public static String getDefaultDomain() {
-        return SharedPreUtils.getInstance().getString("domain", "fycz.tk");
+        return SharedPreUtils.getInstance().getString("domain", "fycz.me");
     }
 
 }
